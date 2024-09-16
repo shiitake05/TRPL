e057351495f9e435836b20632f44c2ea7441b276
k22082kk <k22082kk@k22082kknoMacBook-Air.local>
2024-09-17 02:34:53 +0900

    add: Study Rust arrays

---
+++ arry/target/debug/arry.d
@@ -0,0 +1,1 @@
+/Users/k22082kk/3_programming/Rust/arry/target/debug/arry: /Users/k22082kk/3_programming/Rust/arry/src/main.rs
---
+++ arry/target/debug/deps/arry-2632d2d6fd609abe.d
@@ -0,0 +1,5 @@
+/Users/k22082kk/3_programming/Rust/arry/target/debug/deps/arry-2632d2d6fd609abe: src/main.rs
+
+/Users/k22082kk/3_programming/Rust/arry/target/debug/deps/arry-2632d2d6fd609abe.d: src/main.rs
+
+src/main.rs:
