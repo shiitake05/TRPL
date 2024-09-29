9a29494bc0d81bbc4354adeecc2900f2a6069b05
shiitake05 <pegtar377512@gmail.com>
2024-09-30 00:52:17 +0900

    Stady Rust panic

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,41 +1,9 @@
-use std::fs::File;
-use std::io::ErrorKind;
+// fn main() {
+//     panic!("crash and burn");  //クラッシュして炎上
+// }
 
 fn main() {
-    let f = File::open("hello.txt");
-    // let f: u32 = File::open("hello.txt");
+    let v = vec![1, 2, 3];
 
-    // let f = match f {
-    //     Ok(file) => file,
-    //     Err(error) => {
-    //         // ファイルを開く際に問題がありました
-    //         panic!("There was a problem opening the file: {:?}", error)
-    //     },
-    // };
-
-    let f = match f {
-        Ok(file) => file,
-        Err(ref error) if error.kind() == ErrorKind::NotFound => {
-            match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => {
-                    panic!(
-                        //ファイルを作成しようとしましたが、問題がありました
-                        "Tried to create file but there was a problem: {:?}",
-                        e
-                    )
-                },
-            }
-        },
-        Err(error) => {
-            panic!(
-                "There was a problem opening the file: {:?}",
-                error
-            )
-        },
-    };
-
-
-    // 
-    let f = File::open("hello.txt").unwrap();
+    v[99];
 }
